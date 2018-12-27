0b0229984e6cb233192466fe56a8f89340405ad3
Dave Giunta <dgiunta@mac.com>
2018-12-27 11:57:06 -0600

    wip

--- lib/codeship_api.rb
+++ lib/codeship_api.rb
@@ -1,6 +1,35 @@
 require "codeship_api/version"
+require 'net/http'
+require 'json'
+require 'pry'
 
 module CodeshipApi
+  ROOT = 'https://api.codeship.com/v2/'
   class Error < StandardError; end
   # Your code goes here...
+
+  def self.token
+    @token ||= get_token
+  end
+
+  def self.get_token
+    post("/auth")
+  end
+
+  private
+
+  def self.auth_post(path, params={}, headers={})
+    post(path, params, headers.merge({"Authorization" => "Bearer #{token}"}))
+  end
+
+  def self.post(path, params={}, headers={})
+    uri = URI(ROOT + path.sub(/^\//, ''))
+
+    headers = {
+      "Content-Type" => "application/json",
+      "Accept" => "application/json"
+    }.merge(headers)
+
+    res = Net::HTTP.post(uri, params.to_json, headers)
+  end
 end
