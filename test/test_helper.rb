0a12fe226dd7fc86ee35e278b37d4e87e94f123d
Dave Giunta <dgiunta@mac.com>
2018-12-31 18:02:23 -0600

    updating test_helper with some minitest config and adding a default .env.test file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,12 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
+require 'pry'
+require 'dotenv'
+Dotenv.load('.env', '.env.test')
+
 require "codeship_api"
 
+require "minitest/spec"
+require "minitest/mock"
+require "minitest/pride"
 require "minitest/autorun"
