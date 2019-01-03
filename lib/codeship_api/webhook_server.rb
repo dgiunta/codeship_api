11e3a2e911c4f82c6d6345f5dd15b4d6eaf4d0e6
Dave Giunta <dgiunta@mac.com>
2019-01-03 11:35:34 -0600

    making webhook tolerant of branch deletions

--- lib/codeship_api/webhook_server.rb
+++ lib/codeship_api/webhook_server.rb
@@ -38,11 +38,13 @@ module CodeshipApi
       request.body.rewind
       data = JSON.parse(request.body.read)
 
-      ref = data['ref'].sub(/^refs\//, '')
-      repo_url = data['repository']['html_url']
-      commit_sha = data['head_commit']['id']
+      unless data['deleted']
+        ref = data['ref'].sub(/^refs\//, '')
+        repo_url = data['repository']['html_url']
+        commit_sha = data['head_commit']['id'] if data['head_commit']
 
-      ProcessWebhookJob.perform_async(repo_url, ref, commit_sha)
+        ProcessWebhookJob.perform_async(repo_url, ref, commit_sha)
+      end
 
       [201, {'Content-Type' => 'application/json'}, {'status': 'OK'}.to_json]
     end
